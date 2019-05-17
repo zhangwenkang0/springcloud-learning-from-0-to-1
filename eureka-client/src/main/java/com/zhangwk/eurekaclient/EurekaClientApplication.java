6b1ffe7daf3b62bfa3249fd852ee20bc2094ee97
zhangwenkang <zhangwenkang@hqjy.com>
2019-05-17 09:12:19 +0800

    Hystrix例子

--- eureka-client/src/main/java/com/zhangwk/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/zhangwk/eurekaclient/EurekaClientApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
